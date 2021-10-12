8b35d133861f9a4fb55467675e4f4f52ada73fb5
Brandonarmand96 <66756596+Brandonarmand96@users.noreply.github.com>
2021-10-12 15:58:19 +0100

    finished app
    finished app

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import CreateArea from "./createArea";
+import { createNotEmittedStatement } from "typescript";
 
 
 function App() {
-    return ( <
+
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(note) {
+        setNotes(prevNotes => {
+            retun[...prevNotes, newNote];
+        });
+    }
+    return <
         div >
         <
-        Header / >
+        Header / > //{
+        // Keep.map((noteItem) => <
+        //    Note key = { noteItem.key }
+        //    title = { noteItem.title }
+        //     content = { noteItem.content }
+        //     / > )} <
+        //     Footer / >
+        //     <
+        //     /div>
+        // );
+        //}
         <
-        Note / >
-        <
-        Footer / >
-        <
-        /div>
-    );
+        CreateArea onAdd = { addNote }
+    / > {notes.map((noteItem) => {
+    return <Note
+    title = { noteItem.title }
+    content = { noteItem.content }
+})
+} <
+Note key = { 1 }
+title = "Note title"
+content = "Note content" / >
+    <
+    Footer / >
+    <
+    /div>
+);
 }
 
 export default App;
---
+++ src/components/keep.js
@@ -0,0 +1,23 @@
+const Keep = [{
+    key: 1,
+    title: "Secret 1",
+    content: "really i dont know what to do but am trying hard"
+}, {
+    key: 2,
+    title: "Secret 2",
+    content: "really i dont know what to do but am trying hard"
+}, {
+    key: 3,
+    title: "Secret 3",
+    content: "really i dont know what to do but am trying hard"
+}, {
+    key: 4,
+    title: "Secret 4",
+    content: "really i dont know what to do but am trying hard"
+}, {
+    key: 5,
+    title: "Secret 5",
+    content: "really i dont know what to do but am trying hard"
+}]
+
+export default Keep;
